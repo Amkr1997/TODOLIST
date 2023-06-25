88771eef8e678fc81c734335d8cde0871aee0602
Aman Khare <113411805+Amkr1997@users.noreply.github.com>
2023-06-25 17:13:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <Fragment>
+      <Todo />
+    </Fragment>
+  );
+}
+
+export default App;
